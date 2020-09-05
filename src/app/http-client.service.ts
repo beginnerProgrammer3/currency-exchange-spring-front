7ff467504095fe786741f825448155d8871a3a88
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-09-05 20:50:30 +0200

    added proxy conf

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -16,21 +16,21 @@ export class HttpClientService {
   ) { }
 
   getAllCurrences(){
-    return this.httpClient.get<Currency[]>('http://localhost:8080/api/getallcurrences');
+    return this.httpClient.get<Currency[]>('/api/getallcurrences');
   }
 
   getActualCurrencyRate(from: string, to: string){
-    return this.httpClient.get('http://localhost:8080/api/getcurrencyexchangerate/' + from + '/exchange/' + to);
+    return this.httpClient.get('/api/getcurrencyexchangerate/' + from + '/exchange/' + to);
   }
 
   getDailyHistoryOfCurrenciesRate(from: string, to: string){
-    return this.httpClient.get<HistoryData[]>('http://localhost:8080/api/charts/daily/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('/api/charts/daily/' + from + '/and/' + to);
   }
   getWeeklyHistoryOfCurrenciesRate(from: string, to: string){
-    return this.httpClient.get<HistoryData[]>('http://localhost:8080/api/charts/weekly/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('/api/charts/weekly/' + from + '/and/' + to);
   }
   getMonthlyHistoryOfCurrenciesRate(from: string, to: string): Observable<HistoryData[]>{
-    return this.httpClient.get<HistoryData[]>('http://localhost:8080/api/charts/monthly/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('/api/charts/monthly/' + from + '/and/' + to);
   }
 
 
