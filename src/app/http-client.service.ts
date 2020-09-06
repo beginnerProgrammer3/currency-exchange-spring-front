77109db8803f2526ac32b415cf3e659e2c80c30a
beginnerProgrammer3 <jaceksko91@gmail.com>
2020-09-06 14:56:08 +0200

    update to deploy on heroku                                                                           -working with cors error#3

--- src/app/http-client.service.ts
+++ src/app/http-client.service.ts
@@ -15,21 +15,21 @@ export class HttpClientService {
   ) { }
 
   getAllCurrences(){
-    return this.httpClient.get<Currency[]>('https://currencybackendjs.herokuapp.com/api/getallcurrences');
+    return this.httpClient.get<Currency[]>('http://currencybackendjs.herokuapp.com/api/getallcurrences');
   }
 
   getActualCurrencyRate(from: string, to: string){
-    return this.httpClient.get('https://currencybackendjs.herokuapp.com/api/getcurrencyexchangerate/' + from + '/exchange/' + to);
+    return this.httpClient.get('http://currencybackendjs.herokuapp.com/api/getcurrencyexchangerate/' + from + '/exchange/' + to);
   }
 
   getDailyHistoryOfCurrenciesRate(from: string, to: string){
-    return this.httpClient.get<HistoryData[]>('https://currencybackendjs.herokuapp.com/api/charts/daily/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('http://currencybackendjs.herokuapp.com/api/charts/daily/' + from + '/and/' + to);
   }
   getWeeklyHistoryOfCurrenciesRate(from: string, to: string){
-    return this.httpClient.get<HistoryData[]>('https://currencybackendjs.herokuapp.com/api/charts/weekly/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('http://currencybackendjs.herokuapp.com/api/charts/weekly/' + from + '/and/' + to);
   }
   getMonthlyHistoryOfCurrenciesRate(from: string, to: string): Observable<HistoryData[]>{
-    return this.httpClient.get<HistoryData[]>('https://currencybackendjs.herokuapp.com/api/charts/monthly/' + from + '/and/' + to);
+    return this.httpClient.get<HistoryData[]>('http://currencybackendjs.herokuapp.com/api/charts/monthly/' + from + '/and/' + to);
   }
 
 
